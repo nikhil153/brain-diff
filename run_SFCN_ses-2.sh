35de810633f3d133377441de17921d54cd34fd50
nikhil153 <nikhil153@gmail.com>
2021-10-20 13:36:59 -0400

    removed duplicates

--- run_SFCN_ses-2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 run_SFCN.py \
---data_dir "/neurohub/ukbb/imaging/T1/" \
---img_subdir "ses-2/non-bids/T1/" \
---sfcn_ckpt "models/run_0/lsn.ckpt" \
---subject_list "subject_lists/ukbb_brain-age_follow-up_subjects.txt" \
---scan_session "ses-2" \
---save_path "./brain-age_ukbb-followup_ses-2_LSN-run-1_results.csv"
