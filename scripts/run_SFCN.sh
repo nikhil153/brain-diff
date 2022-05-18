4840473f540f69f5c81824f6f07e7839a3afe030
nikhil153 <nikhil153@gmail.com>
2022-05-17 22:56:32 -0400

    started adni prediction with SFCN

--- scripts/run_SFCN.sh
+++ scripts/run_SFCN.sh
@@ -2,11 +2,12 @@
 
 pip install -e ../
 
-python3 src/run_SFCN.py \
---data_dir "/neurohub/ukbb/imaging/T1/" \
---sfcn_ckpt "models/run_20190719_00_epoch_best_mae.p" \
---subject_list "./test_subject_list.txt" \
---scan_session "ses-2" \
+python3 ../src/run_SFCN.py \
+--data_dir "/home/nikhil/projects/brain_changes/data/adni/imaging/fmriprep/" \
+--img_subdir "ses-bl/anat/" \
+--sfcn_ckpt "../models/run_20190719_00_epoch_best_mae.p" \
+--subject_list "ADNI941S5193" \
+--cohort "adni" \
+--scan_session "ses-bl" \
 --save_path "./tmp_results.csv"
 # --save_path "./brain-age_ukbb-followup_ses-2_SFCN-run-1_results.csv"
-
